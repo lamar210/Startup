b7d8e3b6fc2684967ea7b70e3d4e2fa8d77cb69b
lamar210 <lms210@byu.edu>
2024-11-06 23:29:40 -0700

    update

---
+++ src/about/NavButton.js
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button';
+
+export function NavButton({ text, url }) {
+  const navigate = useNavigate();
+  return (
+    <Button variant='primary' onClick={() => navigate({ url })}>
+      {text}
+    </Button>
+  );
+}
---
+++ src/about/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function About() {
+  return (
+    <main className='container-fluid bg-info text-center'>
+      <div>about displayed here</div>
+    </main>
+  );
+}
