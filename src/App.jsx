97a132f6edab5c563e0bd28ef03f8470ec8b3fac
lamar210 <lms210@byu.edu>
2024-11-16 02:30:44 -0700

    fixing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Journal from './pages/Journal';
-import VibeChecker from './pages/VibeChecker';
+import Journal from './pages/journal';
+import VibeChecker from "./pages/vibe_checker";
 import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Signup from './pages/create_account';
 import Header from './components/Header';
 
 const App = () => {
@@ -13,9 +13,9 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/create_account" element={<Signup />} />
           <Route path="/journal" element={<Journal />} />
-          <Route path="/vibe-checker" element={<VibeChecker />} />
+          <Route path="/vibe_checker" element={<VibeChecker />} />
         </Routes>
       </div>
     </Router>
